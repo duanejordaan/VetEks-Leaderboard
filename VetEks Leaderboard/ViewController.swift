7f081713a0d9df41bd45451108fa2375ea42de74
duanejordaan <duane.jordaan@gmail.com>
2017-07-08 11:25:29 +0200

    Initial Commit

---
+++ VetEks Leaderboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VetEks Leaderboard
+//
+//  Created by Duane Jordaan on 2017/07/08.
+//  Copyright © 2017 Duane Jordaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
