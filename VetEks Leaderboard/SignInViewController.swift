a0835837d9075bf43579b4d9cda76b14a67bf6ef
duanejordaan <duane.jordaan@gmail.com>
2017-07-08 11:48:07 +0200

    second commit

---
+++ VetEks Leaderboard/SignInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignInViewController.swift
+//  VetEks Leaderboard
+//
+//  Created by Duane Jordaan on 2017/07/08.
+//  Copyright © 2017 Duane Jordaan. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
